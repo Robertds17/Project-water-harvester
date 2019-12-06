694e7a4feb8d8cb6f7e4d8767bb34e5cb054475b
Robert de Smit <robertdesmit@hotmail.nl>
2019-12-06 11:43:04 +0100

    project first build

---
+++ app/src/main/java/com/example/water_harvester/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.water_harvester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
