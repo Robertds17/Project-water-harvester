83e6a9d69f61ec19545c06b46131370c52a1a4aa
Robert de Smit <robertdesmit@hotmail.nl>
2019-12-10 12:39:02 +0100

    added activities

---
+++ android/app/src/main/java/com/example/water_harvester/StatisticsActivity.java
@@ -0,0 +1,16 @@
+package com.example.water_harvester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+
+        String title = "Statistics";
+    }
+}
