f58882ed5eb3c2af434151b25f9dd35883554f06
Stefan <stefan730@hotmail.nl>
2020-01-06 13:08:21 +0100

    Fragment logica

--- android/app/src/main/java/com/example/water_harvester/MainActivity.java
+++ android/app/src/main/java/com/example/water_harvester/MainActivity.java
@@ -44,162 +44,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentFirebaseData()).commit();
+
         // test stuff here
 
 
         // stop testing stuff here
 
 
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-        final DatabaseReference dbRef = database.getReference();
-        final DatabaseReference ambientTempRef = dbRef.child("ambientTemp");
-        final DatabaseReference fanRPMRef = dbRef.child("fanRPM");
-        final DatabaseReference insideHUMRef = dbRef.child("insideHum");
-        final DatabaseReference insideTempRef = dbRef.child("insideTemp");
-        final DatabaseReference ambientHumRef = dbRef.child("ambientHum");
-        final DatabaseReference onOffRef = dbRef.child("onOff");
-
-        final TextView seekbarRPMView = findViewById(R.id.rpmspeed);
-        final Switch onOffSwitchView = findViewById(R.id.switch1);
-        final Button fanChangeView = findViewById(R.id.fanChange);
-        final SeekBar seekbarView = findViewById(R.id.seekBar);
-        final EditText editFanRPMView = findViewById(R.id.editRPM);
-        final PieChartView ambientTempView = findViewById(R.id.ambientTempView);
-        final PieChartView fanRPMView = findViewById(R.id.fanRPMView);
-        final PieChartView insideHUMView = findViewById(R.id.InsideHumView);
-        final PieChartView insideTempView = findViewById(R.id.InsideTempView);
-        final PieChartView ambientHumView = findViewById(R.id.ambientHumView);
-
-
-        dbRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                DataStructure value = dataSnapshot.getValue(DataStructure.class);
-                Log.d("Value", "Value: " + value);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w("Value", "Failed to read value with error: ", databaseError.toException());
-            }
-        });
-
-
-        onOffSwitchView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.v("Switch State", "" + isChecked);
-                onOffRef.setValue(isChecked);
-            }
-        });
-
-        fanChangeView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                int fanRpmConvert;
-                try{
-                    fanRpmConvert =  Integer.parseInt(editFanRPMView.getText().toString());
-                } catch (NumberFormatException ex){
-                    fanRpmConvert = 0;
-                }
-                Log.v("changeRPM", "" + fanRpmConvert);
-                fanRPMRef.setValue(fanRpmConvert);
-            }
-        });
-
-
-        seekbarView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                seekbarRPMView.setText(String.valueOf(progress));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                int seekbarRpmConvert = seekBar.getProgress();
-                Log.v("RPM slider:", String.valueOf(seekbarRpmConvert));
-                fanRPMRef.setValue(seekbarRpmConvert);
-            }
-        });
-
-        ambientTempRef.addValueEventListener(new ValueEventListener() {
-             @Override
-             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                 long value = (long)dataSnapshot.getValue();
-                 Log.d("ambientTemp","" + value);
-                 ambientTempView.setPieChartData(BuildAmbientTempPieChartData(value, getColorFromResource(R.color.lightOrange)));
-             }
-
-             @Override
-             public void onCancelled(@NonNull DatabaseError databaseError) {
-                 Log.w("ambientTemp", "Failed to read value with error: ", databaseError.toException());
-             }
-         });
-
-        fanRPMRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                long value = (long)dataSnapshot.getValue();
-                Log.d("fanRPMRef","fanRPMRef: " + value);
-                fanRPMView.setPieChartData(BuildFanRPMPieChartData(value, getColorFromResource(R.color.lightYellow)));
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w("fanRPMRef", "Failed to read value with error: ", databaseError.toException());
-            }
-        });
-
-        insideHUMRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                long value = (long)dataSnapshot.getValue();
-                Log.d("insideHUMRef","insideHUMRef: " + value);
-                insideHUMView.setPieChartData(BuildInsideHumPieChartData(value, getColorFromResource(R.color.lightBlue)));
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w("insideHUMRef", "Failed to read value with error: ", databaseError.toException());
-            }
-        });
-
-        insideTempRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                long value = (long)dataSnapshot.getValue();
-                Log.d("insideTempRef","insideTempRef: " + value);
-                insideTempView.setPieChartData(BuildInsideTempPieChartData(value, getColorFromResource(R.color.lightOrange)));
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w("insideTempRef", "Failed to read value with error: ", databaseError.toException());
-            }
-        });
-
-        ambientHumRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                long value = (long)dataSnapshot.getValue();
-                Log.d("ambientHumRef","ambientHumRef: " + value);
-                ambientHumView.setPieChartData(BuildAmbientHumPieChartData(value, getColorFromResource(R.color.lightBlue)));
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w("ambientHumRef", "Failed to read value with error: ", databaseError.toException());
-            }
-        });
     }
 
-    private int getColorFromResource(int id){
-        return ResourcesCompat.getColor(getResources(), id, null);
-    }
+
 }
